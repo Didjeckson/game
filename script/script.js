57bab4fdf34658c10e8b5b53e94e975115e59527
Di-djeckson <didjeckson@gmail.com>
2020-08-28 05:21:16 +0300

    first version without win. with no work isNaN&&GameOver

--- script/script.js
+++ script/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function game() {
+    let gameNumber = Math.floor(Math.random() * 100);
+    if (gameNumber === 0) {
+        game();
+    }
+    console.log('Random number: ' + gameNumber);
+    let userNumber = prompt('Угадай число от 1 до 100');
+
+    function gameMechanic(gamerSolution) {
+
+        if (userNumber === isNaN) {
+            userNumber = prompt('Введи число!');
+        }
+
+        if (userNumber < gameNumber) {
+            userNumber = prompt('Загаданное число больше');
+        } else if (userNumber > gameNumber) {
+            userNumber = prompt('Загаданное число меньше');
+        }
+
+        if (userNumber === false) {
+            alert('game over');
+        }
+
+        if (userNumber !== gameNumber) {
+            gameMechanic(userNumber);
+        }
+
+    }
+
+    gameMechanic(userNumber);
+}
+
+game();
